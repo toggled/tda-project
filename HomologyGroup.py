BUGFIX in k-th homology Group computation.

HomologyGroup.py
@@ -17,42 +17,106 @@ class Homology:
         self.delta_k = boundary_group_k.get_transformation_matrix()
 
         self.delta_kplus1 = boundary_group_kplus1.get_transformation_matrix()
-
+        print self.delta_k
+        print self.delta_kplus1
         self.kernel_basis_delta_k = boundary_group_k.get_columnobjects()
         self.image_basis_delta_kplus1 = boundary_group_kplus1.get_rowobjects()
 
         # self.rowops_delta_k = []*len(self.delta_k.shape[1]) # delta_k is used to compute the kernel dim. i.e num of dependent columns
         # self.rowops_delta_kplus1 = []*len(self.delta_kplus1.shape[0]) # delta_kplus1 is used to compute the image dim, i.e the num of independent rows
 
+    # def compute_kth_homology_groups(self):
+    #     """
+    #     Compute the K-th Homology groups (Hk)
+    #     :return: Tuple (Basis for kernel space(Zk) and Basis for Image space (Bk)
+    #     TODO:   BUG HERE.
+    #     """
+    #
+    #     # print self.kernel_basis_delta_k
+    #     # print self.image_basis_delta_kplus1
+    #
+    #     def get_kernel_space(reduced_matrix, z_k):
+    #         zeros = np.zeros(reduced_matrix.shape[1])
+    #         l = [i for i in range(reduced_matrix.shape[0]) if np.all(reduced_matrix[i, :] == zeros) == True]
+    #         return z_k[l]
+    #
+    #     reduced_mat2, operationsets2 = self.get_reduced_reform(self.delta_k.T)
+    #     kernel_spaces = self.interpret_rref(self.kernel_basis_delta_k, operationsets2)
+    #     kernel_generators = get_kernel_space(reduced_mat2, np.array(kernel_spaces))
+    #     print 'x ',operationsets2
+    #     print 'hi ',reduced_mat2
+    #     for x in kernel_generators:
+    #         print x
+    #     def get_basis_imagespace(reduced_matrix): # Find the pivot columns
+    #         #zeros = np.zeros(reduced_matrix.shape[1])
+    #         #l = [i for i in range(reduced_matrix.shape[0]) if np.all(reduced_matrix[i, :] == zeros) == False]
+    #         #return b_k[l]
+    #         print self.image_basis_delta_kplus1
+    #         basis_list = []
+    #         for i in range(reduced_mat.shape[0]):
+    #             for k in range(i,reduced_mat.shape[1]):
+    #                 if reduced_mat[i][k] == 1:
+    #                     basis_list.append(self.image_basis_delta_kplus1[k])
+    #                     break
+    #         return basis_list
+    #
+    #     reduced_mat, operationsets = self.get_reduced_reform(self.delta_kplus1)  # Find the image space (span of Bk)
+    #     print self.delta_kplus1
+    #     print reduced_mat
+    #     print operationsets
+    #
+    #     image_spaces = self.interpret_rref(self.image_basis_delta_kplus1, operationsets)
+    #
+    #     generators_imagespace = get_basis_imagespace(reduced_mat) # To find the image space we don't need the basis for rows.
+    #                                                              # we need to find pivot columns i.e minimum columns that are enough to represent image space
+    #     import sys
+    #     sys.exit(1)
+    #     # print image_spaces
+    #
+    #     return (kernel_generators, generators_imagespace)
+
     def compute_kth_homology_groups(self):
-        """
-        Compute the K-th Homology groups (Hk)
-        :return: Tuple (Basis for kernel space(Zk) and Basis for Image space (Bk)
-        """
-
-        # print self.kernel_basis_delta_k
-        # print self.image_basis_delta_kplus1
-
-        def get_basis_imagespace(reduced_matrix, b_k):
-            zeros = np.zeros(reduced_matrix.shape[1])
-            l = [i for i in range(reduced_matrix.shape[0]) if np.all(reduced_matrix[i, :] == zeros) == False]
-            return b_k[l]
-
-        reduced_mat, operationsets = self.get_reduced_reform(self.delta_kplus1)  # Find the image space (span of Bk)
-        image_spaces = self.interpret_rref(self.image_basis_delta_kplus1, operationsets)
-        generators_imagespace = get_basis_imagespace(reduced_mat, np.array(image_spaces))
-
-        # print image_spaces
 
         def get_kernel_space(reduced_matrix, z_k):
-            zeros = np.zeros(reduced_matrix.shape[1])
-            l = [i for i in range(reduced_matrix.shape[0]) if np.all(reduced_matrix[i, :] == zeros) == True]
-            return z_k[l]
+            zeros = np.zeros(reduced_matrix.shape[0], dtype=np.int)
+            l = [i for i in range(reduced_matrix.shape[1]) if np.all(reduced_matrix[:, i] == zeros) == True]
+            return list(z_k[l])
 
-        reduced_mat2, operationsets2 = self.get_reduced_reform(self.delta_k.T)
-        kernel_spaces = self.interpret_rref(self.kernel_basis_delta_k, operationsets2)
-        kernel_generators = get_kernel_space(reduced_mat2, np.array(kernel_spaces))
-        return (kernel_generators, generators_imagespace)
+        def get_basis_imagespace(reduced_mat, image_basis_space):  # Find the pivot columns
+
+            basis_list = []
+            for i in range(reduced_mat.shape[0]):
+                for k in range(i, reduced_mat.shape[1]):
+                    if reduced_mat[i][k] == 1:
+                        basis_list.append(image_basis_space[i])
+                        break
+            return basis_list
+
+        rowops, colops, dk, dk_1 = self.simultaneousReduce(self.delta_k, self.delta_kplus1)
+        # print dk
+        dk_1, rowops = self.get_reduced_reform(dk_1, rowops)
+        # print dk_1
+
+        kernel_spaces = self.interpret_rref(self.kernel_basis_delta_k, colops)
+
+        kernel_generators = get_kernel_space(dk, np.array(kernel_spaces))
+        print kernel_generators
+        self.play_with_kernel_space(kernel_generators)
+
+        image_spaces = self.interpret_rref(self.image_basis_delta_kplus1, rowops)
+        image_generators = get_basis_imagespace(dk_1, np.array(image_spaces))
+        # print image_generators
+        return (kernel_generators, image_generators)
+
+    def play_with_kernel_space(self, kernel_generators):
+        for i in range(len(kernel_generators)):
+            for j in range(i + 1, len(kernel_generators)):
+                if i == 2 or j == 2:
+                    continue
+                diff = kernel_generators[i] - kernel_generators[j]
+                print diff, (kernel_generators[i], kernel_generators[j])
+
+
 
     def compute_betti_number(self):
 
@@ -119,7 +183,10 @@ class Homology:
                 # if type(operations_list) is list:
                 #     string_repr[operations_list[0]] = str(operations_list[0])
                 #     return operations_list[0], string_repr[operations_list[0]]
-                return operations_list[0], string_repr[operations_list[0]]
+                return operations_list[0], Symbol(repr_row_simplices[operations_list[
+                    0]])  # operation list is operation on row in transformation matrix
+                # We need to return the corresponding symbolic representation
+                # of the simplex at that row.
                     # if type(operations_list) is tuple:
                     #     idx, op = process_next_op(operations_list[0])
                     #     return idx,string_repr[idx]
@@ -133,14 +200,61 @@ class Homology:
             #print string_repr
         return string_rowrepr
 
-    def get_reduced_reform(self, input_matrix):
+    def simultaneousReduce(self, A, B):
+        # print 'inside simultaneous reduce\n'
+
+        row_operations_list = [[i] for i in range(B.shape[0])]  # [] * number of rows
+        col_operations_list = [[i] for i in range(A.shape[1])]  # [] * number of cols
+
+        if A.shape[1] != B.shape[0]:
+            raise Exception("Matrices have the wrong shape.")
+
+        numRows, numCols = A.shape
+
+        i, j = 0, 0
+        operation = 1
+        while True:
+            if i >= numRows or j >= numCols:
+                break
+
+            if A[i, j] == 0:
+                nonzeroCol = j
+                while nonzeroCol < numCols and A[i, nonzeroCol] == 0:
+                    nonzeroCol += 1
+
+                if nonzeroCol == numCols:
+                    i += 1
+                    continue
+
+                self.colSwap(A, col_operations_list, j, nonzeroCol)
+                self.rowSwap(B, row_operations_list, j, nonzeroCol)
+
+            pivot = A[i, j]
+            self.scaleCol(A, col_operations_list, j, 1 * pivot)
+            self.scaleRow(B, row_operations_list, j, 1 * pivot)
+
+            for otherCol in range(0, numCols):
+                if otherCol == j:
+                    continue
+                if A[i, otherCol] != 0:
+                    scaleAmt = -A[i, otherCol]
+                    self.colCombine(A, col_operations_list, otherCol, j, scaleAmt)
+                    self.rowCombine(B, row_operations_list, j, otherCol, -scaleAmt)
+
+            i += 1
+            j += 1
+
+        # print 'exiting simultaneous reduce\n'
+        return (row_operations_list, col_operations_list, A, B)
+
+    def get_reduced_reform(self, input_matrix, operations_list=[]):
         """
         Performs Elementary Row reduction- converting input_matrix into reduced-row-echelon-form(rref)
         :param input_matrix:
-        :return: Reduced form of the input_matrix, list of operation sets performed on each rows
+        :return: Reduced form of the input_matrix, list of operation sets performed on each rows. i.e the operation list
         """
-
-        operations_list = [[i] for i in range(input_matrix.shape[0])]  # [] * number of rows
+        if operations_list == []:
+            operations_list = [[i] for i in range(input_matrix.shape[0])]  # [] * number of rows
 
         matrix = deepcopy(input_matrix)
         numrows, numcols = matrix.shape
@@ -177,8 +291,9 @@ class Homology:
         return matrix, operations_list
 
     def rowSwap(self, A, rowops_list, i, j):
-        temp, tempop = np.copy(A[i, :]), deepcopy(rowops_list[i])
+        temp, tempop = np.copy(A[i, :]), list(deepcopy(rowops_list[i]))
         A[i, :], rowops_list[i] = A[j, :], rowops_list[j]
+        assert isinstance(tempop, list)
         A[j, :], rowops_list[j] = temp, tempop
 
     def scaleRow(self, A, rowops_list, i, c):
@@ -192,3 +307,16 @@ class Homology:
     def numPivotRows(self, A):
         z = np.zeros(A.shape[1])
         return [np.all(A[i, :] == z) for i in range(A.shape[0])].count(False)
+
+    def colSwap(self, A, colops_list, i, j):
+        temp, tempop = np.copy(A[:, i]), list(deepcopy(colops_list[i]))
+        A[:, i], colops_list[i] = A[:, j], colops_list[j]
+        A[:, j], colops_list[j] = temp, tempop
+
+    def scaleCol(self, A, colops_list, i, c):
+        A[:, i] *= c * np.ones(A.shape[0], dtype=np.int)
+        colops_list[i] = (colops_list[i], c)
+
+    def colCombine(self, A, colops_list, addTo, scaleCol, scaleAmt):
+        A[:, addTo] += scaleAmt * A[:, scaleCol]
+        colops_list[addTo] = (colops_list[addTo], (colops_list[scaleCol], scaleAmt), '+')